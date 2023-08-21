13d8b0db903fc8080799825ed5069ce480aaaea7
Clément Lopez <clement@urbanease.io>
2023-08-21 16:56:12 +0200

    Initial Commit

---
+++ Clicker/ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerApp.swift
+//  Clicker
+//
+//  Created by Clément Lopez on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
